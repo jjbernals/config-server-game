0f97df2ea57cfea42eb5cc5824fac4617d54a0d7
John <john.bernal11@misena.edu.co>
2023-06-07 11:51:03 -0500

    Initial commit

---
+++ src/main/java/com/example/configservergame/ConfigServerGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.configservergame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/configservergame/ConfigServerGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configservergame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
